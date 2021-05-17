7b7895a95481333d4d816ffeb809648aa571d980
Ezure-HARUNA <haruna.e@80and.co>
2021-05-17 12:04:00 +0900

    style:四つのメニューバー作成

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoryBar from './components/CategoryBar'
+
+const App = () => {
+  return (
+    <div>
+      <CategoryBar />
+    </div>
+  )
+}
+
+export default App
