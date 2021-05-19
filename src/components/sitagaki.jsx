5690695b1ce1961e389f842a46e2d4b262494a03
Ezure-HARUNA <haruna.e@80and.co>
2021-05-19 15:53:23 +0900

    style:レスポンシブの切り替え成功

--- src/components/sitagaki.jsx
+++ src/components/sitagaki.jsx
@@ -1,35 +1,26 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  makeStyles,
-  Button,
-  IconButton,
-  Drawer,
-  // Link,
-  MenuItem,
-  useTheme,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState, useEffect } from "react";
-import { 　Link  } from "react-router-dom";
-
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
-import clsx from 'clsx';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
+import React, { useState, useEffect } from "react";
+import { 　Link  } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  makeStyles,
+  IconButton,
+  Drawer,
+  useTheme,
+  CssBaseline,
+  List,
+  Divider,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import clsx from 'clsx';
 import LogoImg from '../assets/img/logo.png';
-// import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -139,58 +130,6 @@ const navLink = css({
   letterSpacing: 0.15,
 })
 
-//いらない
-// const headersData = [
-//   {
-//     label: "Listings",
-//     href: "/listings",
-//   },
-//   {
-//     label: "Mentors",
-//     href: "/mentors",
-//   },
-//   {
-//     label: "My Account",
-//     href: "/account",
-//   },
-//   {
-//     label: "Log Out",
-//     href: "/logout",
-//   },
-// ];
-
-//いらない
-// const useStyles = makeStyles(() => ({
-//   header: {
-//     backgroundColor: "#400CCC",
-//     paddingRight: "79px",
-//     paddingLeft: "118px",
-//     "@media (max-width: 900px)": {
-//       paddingLeft: 0,
-//     },
-//   },
-//   logo: {
-//     fontFamily: "Work Sans, sans-serif",
-//     fontWeight: 600,
-//     color: "#FFFEFE",
-//     textAlign: "left",
-//   },
-//   menuButton: {
-//     fontFamily: "Open Sans, sans-serif",
-//     fontWeight: 700,
-//     size: "18px",
-//     marginLeft: "38px",
-//   },
-//   toolbar: {
-//     display: "flex",
-//     justifyContent: "space-between",
-//   },
-//   drawerContainer: {
-//     padding: "20px 30px",
-//   },
-// }));
-
-// ここだけ残す
 const NavBar = () => {
   const classes = useStyles();
 
@@ -263,132 +202,61 @@ const NavBar = () => {
   };
   //ここにSP版の見た目
   const displayMobile = () => {
-    // const handleDrawerOpen = () =>
-    //   setState((prevState) => ({ ...prevState, drawerOpen: true }));
-    // const handleDrawerClose = () =>
-    //   setState((prevState) => ({ ...prevState, drawerOpen: false }));
-
-    
-  // const [open, setOpen] = useState(false);
-
-  // const handleDrawerOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleDrawerClose = () => {
-  //   setOpen(false);
-  // };
-
-      return (
-        <div className={classes.root}>
-          <CssBaseline />
-          <AppBar
-            position="fixed"
-            className={clsx(classes.appBar, {
-              [classes.appBarShift]: open,
-            })}
-          >
-            <Toolbar css={navWrap}>
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar css={navWrap}>
             <img src={LogoImg} css={logoImg}/>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="end"
-                onClick={handleDrawerOpen}
-                className={clsx(open && classes.hide)}
-              >
-                <MenuIcon style={{ fontSize: 40 }} />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-          <main
-            className={clsx(classes.content, {
-              [classes.contentShift]: open,
-            })}
-          >
-            {/* <div className={classes.drawerHeader} /> */}
-          </main>
-          <Drawer
-            className={classes.drawer}
-            variant="persistent"
-            anchor="right"
-            open={open}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-          >
-            <div className={classes.drawerHeader}>
-              <IconButton onClick={handleDrawerClose}>
-                {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-              </IconButton>
-            </div>
-            <Divider />
-            <List>
-              {['イベント', 'インターン', '会社概要', 'LINE追加'].map((text, index) => (
-                <ListItem button key={text}>
-                  {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
-                  <ListItemText primary={text} margin="0 auto"/>
-                </ListItem>
-              ))}
-            </List>
-            {/* <Divider />
-            <List>
-              {['', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List> */}
-          </Drawer>
-        </div>
-      );
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerOpen}
+              className={clsx(open && classes.hide)}
+            >
+              <MenuIcon style={{ fontSize: 40 }} />
+        　   </IconButton>
+          </Toolbar>
+        </AppBar>
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+        </main>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {['イベント', 'インターン', '会社概要', 'LINE追加'].map((text, index) => (
+              <ListItem button key={text}>
+                {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
+                <ListItemText primary={text} margin="0 auto"/>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </div>
+    );
   };
-
-  // const getDrawerChoices = () => {
-  //   return headersData.map(({ label, href }) => {
-  //     return (
-  //       <Link
-  //         {...{
-  //           component: RouterLink,
-  //           to: href,
-  //           color: "inherit",
-  //           style: { textDecoration: "none" },
-  //           key: label,
-  //         }}
-  //       >
-  //         <MenuItem>{label}</MenuItem>
-  //       </Link>
-  //     );
-  //   });
-  // };
-
-  //いらない
-  const femmecubatorLogo = (
-    <Typography variant="h6" component="h1" className={logo}>
-      Femmecubator
-    </Typography>
-  );
-  
-  //いらない
-  // const getMenuButtons = () => {
-  //   return headersData.map(({ label, href }) => {
-  //     return (
-  //       <Button
-  //         {...{
-  //           key: label,
-  //           color: "inherit",
-  //           to: href,
-  //           // component: RouterLink,
-  //           className: menuButton,
-  //         }}
-  //       >
-  //         {label}
-  //       </Button>
-  //     );
-  //   });
-  // };
-
   return (
     <header>
       <AppBar className={header}>
